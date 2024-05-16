14cbe79cad9ecf523d3298a2bc75048bf759903b
samuelGyasiFordjour <sgfordjour@st.ug.edu.gh>
2024-05-16 17:17:58 +0000

    Added a second function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -2,3 +2,16 @@
 function processArray(arr){
     return arr.map(num => num % 2 === 0 ? num * num : num * 3);
 }
+
+
+function formatArrayStrings(stringsArr, numbersArr){
+        return stringsArr.map((str, index) => {
+            if (numbersArr[index] % 2 === 0) {
+                 return str.toUpperCase();
+                   } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+
